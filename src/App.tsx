74063fc281d7fe3d440d23ca73808ab72201ff03
sharad1996 <sharadwankhade23@gmail.com>
2022-11-25 12:38:02 +0530

    initial routing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./component/Dashboard";
+import TeamDetail from "./component/TeamDetail";
+import UserProfile from "./component/UserProfile";
 
 function App() {
-  return <div className="App">Tempo</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />}></Route>
+      <Route path="/team/:id" element={<TeamDetail />}></Route>
+      <Route path="/user/:id" element={<UserProfile />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
